d3e2d1aadfb93f39fe57d4d3869f18dd327b8abc
progmax646 <2151796qw>
2021-05-11 22:48:39 +0500

    update mobile version on this site

--- meet_legalact/urls.py
+++ meet_legalact/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/meet', permanent=False)),
     path('admin/', admin.site.urls),
     path('meet/', include('meet.urls')),
     path('account/', include('account.urls'))
