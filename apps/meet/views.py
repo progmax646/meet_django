0c91f3a1300bb4c33f6600c280dda9487d1da304
Maksim <maxwork646@gmail.com>
2021-12-02 10:48:25 +0500

    update notification chat id

--- apps/meet/views.py
+++ apps/meet/views.py
@@ -49,12 +49,13 @@ def search_notification(request=None):
         if meet.notification:
             print(meet)
             # -1001296908744
+            # -491996034
             # 7339360
             # 272339311
             try:
                 url = f'https://api.telegram.org/bot{API_TELEGRAM}/sendMessage'
                 params = {
-                    'chat_id': '7339360',
+                    'chat_id': '-491996034',
                     'text': f'❗️Встреча «{meet.client_name}» запланирована на сегодня в {timezone.localtime(meet.date).strftime("%H:%M")}'
                 }
                 requests.get(url=url, params=params)
